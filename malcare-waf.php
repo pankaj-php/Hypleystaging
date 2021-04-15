d33beb6086c5a9dba76b2fbf83df285f79a59c24
michaelbrinkworth <michaelhireitem@gmail.com>
2021-04-16 07:23:26 +1000

    push

---
+++ malcare-waf.php
@@ -0,0 +1,9 @@
+<?php
+// Please validate auto_prepend_file setting before removing this file
+
+if (file_exists('/home/511738.cloudwaysapps.com/wdbgyzpamu/public_html/wp-content/plugins/malcare-security/protect/prepend/ignitor.php')) {
+	define("MCDATAPATH", '/home/511738.cloudwaysapps.com/wdbgyzpamu/public_html/wp-content/mc_data/');
+	define("MCCONFKEY", '28d564bab7fa9c0af6f4feb9df857d5f');
+	include_once('/home/511738.cloudwaysapps.com/wdbgyzpamu/public_html/wp-content/plugins/malcare-security/protect/prepend/ignitor.php');
+}
+?>
---
+++ michael.js
@@ -0,0 +1,1 @@
+michaedl was here

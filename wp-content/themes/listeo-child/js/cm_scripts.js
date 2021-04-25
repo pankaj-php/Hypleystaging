22b9ab4e5b7cb68376d2d9620cdd9d5409c3b0eb
developersajib <44896131+developersajib@users.noreply.github.com>
2021-04-25 12:50:36 +0600

    fix console error and login tab

--- wp-content/themes/listeo-child/js/cm_scripts.js
+++ wp-content/themes/listeo-child/js/cm_scripts.js
@@ -1,4 +1,4 @@
-jQuery(document).ready(function(){
+jQuery(document).ready(function($){
     $('.cm-chat-icon').click(function(){
         $('#send-message-from-widget .notification').hide();
     })

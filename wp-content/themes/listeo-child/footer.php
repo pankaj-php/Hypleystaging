4a1e57729604d9cb2865fa895d60cc04eeece0dd
developersajib <44896131+developersajib@users.noreply.github.com>
2021-04-25 12:49:44 +0600

    fix console error and login tab

--- wp-content/themes/listeo-child/footer.php
+++ wp-content/themes/listeo-child/footer.php
@@ -13,14 +13,14 @@
 
 <!-- Footer
 ================================================== -->
-<?php 
+<?php
 $sticky = get_option('listeo_sticky_footer') ;
 $style = get_option('listeo_footer_style') ;
 
 if(is_singular()){
 
-	$sticky_singular = get_post_meta($post->ID, 'listeo_sticky_footer', TRUE); 
-	
+	$sticky_singular = get_post_meta($post->ID, 'listeo_sticky_footer', TRUE);
+
 	switch ($sticky_singular) {
 		case 'on':
 		case 'enable':
@@ -29,18 +29,18 @@ if(is_singular()){
 
 		case 'disable':
 			$sticky = false;
-			break;	
+			break;
 
 		case 'use_global':
-			$sticky = get_option('listeo_sticky_footer'); 
+			$sticky = get_option('listeo_sticky_footer');
 			break;
-		
+
 		default:
-			$sticky = get_option('listeo_sticky_footer'); 
+			$sticky = get_option('listeo_sticky_footer');
 			break;
 	}
 
-	$style_singular = get_post_meta($post->ID, 'listeo_footer_style', TRUE); 
+	$style_singular = get_post_meta($post->ID, 'listeo_footer_style', TRUE);
 	switch ($style_singular) {
 		case 'light':
 			$style = 'light';
@@ -51,11 +51,11 @@ if(is_singular()){
 			break;
 
 		case 'use_global':
-			$style = get_option('listeo_footer_style'); 
+			$style = get_option('listeo_footer_style');
 			break;
-		
+
 		default:
-			$sticky = get_option('listeo_footer_style'); 
+			$sticky = get_option('listeo_footer_style');
 			break;
 	}
 }
@@ -67,9 +67,9 @@ $sticky = apply_filters('listeo_sticky_footer_filter',$sticky);
 	<div class="container">
 		<div class="row">
 			<?php
-			$footer_layout = get_option( 'pp_footer_widgets','6,3,3' ); 
-			
-	        $footer_layout_array = explode(',', $footer_layout); 
+			$footer_layout = get_option( 'pp_footer_widgets','6,3,3' );
+
+	        $footer_layout_array = explode(',', $footer_layout);
 	        $x = 0;
 	        foreach ($footer_layout_array as $value) {
 	            $x++;
@@ -87,8 +87,8 @@ $sticky = apply_filters('listeo_sticky_footer_filter',$sticky);
 		<!-- Copyright -->
 		<div class="row">
 			<div class="col-md-12">
-				<div class="copyrights"> <?php $copyrights = get_option( 'pp_copyrights' , '&copy; Theme by Purethemes.net. All Rights Reserved.' ); 
-		
+				<div class="copyrights"> <?php $copyrights = get_option( 'pp_copyrights' , '&copy; Theme by Purethemes.net. All Rights Reserved.' );
+
 		            echo wp_kses($copyrights,array( 'a' => array('href' => array(),'title' => array()),'br' => array(),'em' => array(),'strong' => array(),));
 		         ?></div>
 			</div>
@@ -99,7 +99,7 @@ $sticky = apply_filters('listeo_sticky_footer_filter',$sticky);
 <!-- Back To Top Button -->
 <div id="backtotop"><a href="#"></a></div>
 
-<?php if(is_singular('listing')) : 
+<?php if(is_singular('listing')) :
 	$_booking_status = get_post_meta($post->ID, '_booking_status',true);
 	$verify = (get_post_meta($post->ID,'_verified', true));
 	if($_booking_status) : ?>
@@ -107,36 +107,36 @@ $sticky = apply_filters('listeo_sticky_footer_filter',$sticky);
 		<div class="booking-sticky-footer">
 			<div class="container">
 				<div class="bsf-left">
-					<?php 
+					<?php
 					$price_min = get_post_meta( $post->ID, '_price_min', true );
 					if (is_numeric($price_min)) {
 					    $price_min_raw = number_format_i18n($price_min);
-					} 
+					}
 					$currency_abbr = get_option( 'listeo_currency' );
 					$currency_postion = get_option( 'listeo_currency_postion' );
 					$currency_symbol = Listeo_Core_Listing::get_currency_symbol($currency_abbr);
-					
+
 					if($price_min) : ?>
 					<h4><?php esc_html_e('Starting from','listeo') ?> <?php if($currency_postion == 'after') { echo $price_min_raw . $currency_symbol; } else { echo $currency_symbol . $price_min_raw; } ?></h4>
 					<?php else : ?>
 						<h4><?php esc_html_e('Select dates to see prices','listeo') ?></h4>
 					<?php endif; ?>
 
-						<?php 
+						<?php
 						if(!get_option('listeo_disable_reviews')){
-							$rating = get_post_meta($post->ID, 'listeo-avg-rating', true); 
-							if(isset($rating) && $rating > 0 ) : 
+							$rating = get_post_meta($post->ID, 'listeo-avg-rating', true);
+							if(isset($rating) && $rating > 0 ) :
 								$rating_type = get_option('listeo_rating_type','star');
 								if($rating_type == 'numerical') { ?>
 									<div class="numerical-rating" data-rating="<?php $rating_value = esc_attr(round($rating,1)); printf("%0.1f",$rating_value); ?>">
 								<?php } else { ?>
 									<div class="star-rating" data-rating="<?php echo $rating; ?>">
 								<?php } ?>
-									
+
 								</div>
-						<?php endif; 
+						<?php endif;
 						}?>
-					
+
 				</div>
 				<div class="bsf-right">
 					<a href="<?=($verify) ? '#booking-widget-anchor' : '#un_verified_listing_widget'?>" class="button"><?php if($verify){ esc_html_e('Book Now','listeo'); } else { echo "Message Now";} ?></a>
@@ -147,8 +147,8 @@ $sticky = apply_filters('listeo_sticky_footer_filter',$sticky);
 	endif; ?>
 
 </div> <!-- weof wrapper -->
-<?php if(( is_page_template('template-home-search.php') || is_page_template('template-home-search-video.php') || is_page_template('template-home-search-splash.php')) && get_option('listeo_home_typed_status','enable') == 'enable') { 
-	$typed = get_option('listeo_home_typed_text'); 
+<?php if(( is_page_template('template-home-search.php') || is_page_template('template-home-search-video.php') || is_page_template('template-home-search-splash.php')) && get_option('listeo_home_typed_status','enable') == 'enable') {
+	$typed = get_option('listeo_home_typed_text');
 	$typed_array = explode(',',$typed);
 	?>
 						<script src="https://cdn.jsdelivr.net/npm/typed.js@2.0.9"></script>
@@ -167,7 +167,7 @@ $sticky = apply_filters('listeo_sticky_footer_filter',$sticky);
 <?php wp_footer(); ?></body>
 </html>
 <script type="text/javascript">
-$(function(){
+jQuery(function($){
   $('div #media-uploader a').click(function(){
     alert($(this).attr('href'));
   });
